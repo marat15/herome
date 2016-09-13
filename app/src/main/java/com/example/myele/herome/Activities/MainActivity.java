f3523b827aa36ceecd0cbde816b4a6f24d1adf16
marat15 <myelemes@gmail.com>
2016-09-12 19:17:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myele/herome/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myele.herome.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.myele.herome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
